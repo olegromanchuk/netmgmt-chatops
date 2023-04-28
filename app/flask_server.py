c229dd0006a140ceac7aba6b9afb20394ce7c393
Oleg Romanchuk <oleg@romanchuk.us>
2023-04-28 05:13:21 -0400

    add error handling

--- app/flask_server.py
+++ app/flask_server.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from messenger import Messenger
+from restconf import Device
 import datetime as dtc
 import os
 import sys
@@ -28,7 +29,7 @@ def welcomeRoot():
     person_id=data['data']['personId']
 
     app.logger.info(jsonify(request.json))
-    app.logger.info(f"got msg id: {message_id}")
+    app.logger.debug(f"got msg id: {message_id}")
 
     # Get message details
     messenger = Messenger()
@@ -39,13 +40,35 @@ def welcomeRoot():
             action = messenger.message_text.split()[1]
         except IndexError:
             action: 'status'
-        messenger.send_message(room_id, f"performing action: {action}")
-        return
+
+        if action == 'start':
+            app.logger.debug(f"action: start")
+            messenger.send_message(room_id, f"performing action: {action}")
+
+        elif action == 'stop':
+            app.logger.debug(f"action: stop")
+            messenger.send_message(room_id, f"performing action: {action}")
+
+        elif action == 'get_hostname':
+            app.logger.debug(f"action: get_hostname")
+            device = Device("localhost:8443")
+            try:
+                hostname = device.get_hostname()
+                messenger.send_message(room_id, f"hostname: {hostname}")
+            except Exception as e:
+                app.logger.error(f"Error: 7001 failed to get hostname. Error: {e}")
+                messenger.send_message(room_id, f"Error: 7001 failed to get hostname: cannot connect to device")
+        else:
+        # unknown action
+            app.logger.debug(f"unknown action: {action}")
+            messenger.send_message(room_id, f"Unknown action: {action}")
+
+        return "Message processed"
 
     # Post our replay
     #ignore messages from myself
     if person_id == messenger.bot_id:
-        app.logger.info(f"ignore msg from myself")
+        app.logger.debug(f"ignore msg from myself")
         return "Message from myself are ignored"
     else:
         messenger.send_message(room_id, f"got message: {messenger.message_text}")
@@ -61,10 +84,9 @@ def welcomeRoot():
         #                 print ("please check the identifier of the room you're attempting to post to...")
         #         elif res.status_code == 401:
         #                 print ("please check if the access token is correct...")
-    return
+    return "Message processed"
 
 if __name__ == '__main__':
     if os.getenv('BOT_TOKEN') is None or os.getenv('BOT_TOKEN') == '':
-        print("Please set the BOT_TOKEN environment variable")
-        sys.exit(1)
+        raise ValueError ("Please set the BOT_TOKEN environment variable")
     app.run(host='0.0.0.0', port=8000)
---
+++ app/restconf.py
@@ -0,0 +1,54 @@
+import requests
+import os
+import sys
+
+if os.getenv('RESTCONF_USERNAME') is None or os.getenv('RESTCONF_USERNAME') == '' or os.getenv('RESTCONF_PASSWORD') is None or os.getenv('RESTCONF_PASSWORD') == '':
+    raise ValueError('Please set the RESTCONF_USERNAME and RESTCONF_PASSWORD environment variable')
+
+# device's information
+device_username = os.environ["RESTCONF_USERNAME"]
+device_password = os.environ["RESTCONF_PASSWORD"]
+    
+class Device():
+    def __init__(self, device_ip_port, device_username=device_username, device_password=device_password):
+        self.device_username=device_username
+        self.device_password=device_password
+        self.device_ip_port=device_ip_port
+        self.headers = {
+                "Accept": "application/yang-data+json",
+                "Content-Type": "application/yang-data+json"
+            }
+        self.auth=(self.device_username, self.device_password)
+
+    def get_hostname(self):
+        yang_url = f"https://{self.device_ip_port}/restconf/data/Cisco-IOS-XE-native:native/hostname"
+        try:
+            response = requests.get(yang_url, auth=self.auth, headers=self.headers, verify=False)
+        except Exception as e:
+            raise ConnectionError(f"ConnectionError: {e}")
+        
+        return response.json()['Cisco-IOS-XE-native:hostname']
+
+    def get_interfaces(self):
+            # yang_url = f"https://{self.device_ip_port}/restconf/data/Cisco-IOS-XE-native:native/interface/Port-channel=5/ip/address"
+            yang_url = f"https://{self.device_ip_port}/restconf/data/Cisco-IOS-XE-native:native/interface"
+            try:
+                response = requests.get(yang_url, auth=self.auth, headers=self.headers, verify=False)
+            except Exception as e:
+                print(f"Got eerror: {e}")
+            return response.json()
+    
+    def get_models(self):
+        #yang_url = f"https://{device_ip_port}/restconf/data/Cisco-IOS-XE-native:native"
+        #yang_url = f"https://{device_ip_port}/restconf/tailf/modules/Cisco-IOS-XE-interfaces/2019-09-30"
+        yang_url = f"https://{self.device_ip_port}/restconf/data/ietf-yang-library:modules-state"
+        
+        response = requests.get(yang_url, auth=self.auth, headers=self.headers, verify=False)
+        return response.text
+
+
+if __name__ == '__main__':
+    if os.getenv('RESTCONF_USERNAME') is None or os.getenv('RESTCONF_USERNAME') == '' or os.getenv('RESTCONF_PASSWORD') is None or os.getenv('RESTCONF_PASSWORD') == '':
+        print("Please set the RESTCONF_USERNAME and RESTCONF_PASSWORD environment variable")
+        sys.exit(1)
+
