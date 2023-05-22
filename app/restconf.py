a40909cb443dacad49f53317edbbe5f871a801ed
Oleg Romanchuk <oleg@romanchuk.us>
2023-05-22 16:11:53 -0400

    add create port-channel

--- app/restconf.py
+++ app/restconf.py
@@ -3,6 +3,8 @@ import os
 import sys
 import logging
 import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 # Create a custom logger
 restconf_logger = logging.getLogger(__name__)
@@ -27,6 +29,13 @@ device_username = os.environ["RESTCONF_USERNAME"]
 device_password = os.environ["RESTCONF_PASSWORD"]
 device_ip_port = os.environ["DEVICE_IP_PORT"]
     
+class RestConfException(Exception):
+    def __init__(self, message, dict_data):
+        super().__init__(message)
+        self.dict_data = dict_data
+
+
+                      
 class Device():
     def __init__(self, device_ip_port=device_ip_port, device_username=device_username, device_password=device_password, debug=False):
         self.device_username=device_username
@@ -67,23 +76,20 @@ class Device():
             return response.json()
     
 
-    def create_interface_ge(self, interface_name, interface_ip, interface_mask):
+    def create_interface_portchannel(self, interface_name, interface_ip, interface_mask):
         if interface_name is None or interface_name == '':
             raise ValueError('interface_name is required')
         
-        interface_type = 'GigabitEthernet'
+        interface_type = 'Port-channel'
 
         # Define the URL for the POST request
-        yang_url = f"https://{self.device_ip_port}/restconf/data/Cisco-IOS-XE-native:native"
-        restconf_logger.debug(f"sending POST to restconf host. url: {yang_url}, headers:{self.headers}")
-
+        yang_url = f"https://{self.device_ip_port}/restconf/data/Cisco-IOS-XE-native:native/interface/"
        
         # Define the body of the POST request
         payload = {
-            "Cisco-IOS-XE-native:native": {
-                "interface": {
-                    interface_type: [{
+            "Cisco-IOS-XE-native:Port-channel": {
                         "name": interface_name,
+                        "description": interface_name,
                         "ip": {
                             "address": {
                                 "primary": {
@@ -91,29 +97,51 @@ class Device():
                                     "mask": interface_mask
                                 }
                             }
-                        },
-                        "Cisco-IOS-XE-ethernet:negotiation": {
-                            "auto": True
-                        }
-                    }]
-                }
+                        }            
             }
         }
 
+        restconf_logger.debug(f"sending POST to restconf host. url: {yang_url}, headers:{self.headers}, payload: {payload}")
+        
         response = requests.post(yang_url, auth=self.auth, headers=self.headers, json=payload, verify=False)
+        restconf_logger.debug(f"Got result. Status code: {response.status_code}, response_body: {response.text}")
+
+
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
+            restconf_logger.debug(f"Got exception. Status code: {response.status_code}, error: {e}, response_body: {response.json()}")
+            if response.status_code == 400:
+                msgObj = {"error-tag": "invalid-value",
+                              "error-message": response.json()
+                        }
+                raise RestConfException(f"client error. Error: {e}", msgObj)
             if response.status_code == 401:
-                raise ValueError(f"Authentication failed. Please check the RESTCONF_USERNAME and RESTCONF_PASSWORD environment variable. Error: {e}")
+                msgObj = {"error-tag": "authentication-failed",
+                              "error-message": response.json()
+                        }
+                raise RestConfException(f"Authentication failed. Please check the RESTCONF_USERNAME and RESTCONF_PASSWORD environment variable. Error: {e}", msgObj)
+            if response.status_code == 409:
+                msgObj = {"error-tag": "",
+                    "error-message": response.json()
+                    }
+                if response.json()['errors']['error'][0]['error-tag'] == 'data-exists':
+                    msgObj["error-tag"]="data-exists"
+                raise RestConfException(f"client conflict. Error: {e}", msgObj)
             raise ConnectionError(f"ConnectionError: {e}")
         
+        restconf_logger.debug(f"response status code: {response.status_code}, response_body: {response.text}")
+
         # Check if the request was successful
         if response.status_code == 201:
-            return_msg=f"Interface {interface_type} {interface_name} created successfully. Msg: {response.json()}"
+            return_msg=f"Interface {interface_type} {interface_name} created successfully."
         else:
+            msgObj = {"error-tag": "unknown-error",
+                    "error-message": response.text,
+                    "status-code": response.status_code
+                    }
             restconf_logger.error(f"Failed to create interface. Status code: {response.status_code}. Response: {response.text}.")
-            raise ConnectionError(f"Failed to create interface. Status code: {response.status_code}. Response: {response.text}.")
+            raise RestConfException(f"Failed to create interface", msgObj)
 
         return return_msg
 
